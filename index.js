ba0f4bb20dc1712c01478713dda4dfdc4eb9e0e5
theadg <deguzmanbernardandrew@gmail.com>
2023-01-04 17:10:16 +0800

    Add methods to createLinkedList factory

--- index.js
+++ index.js
@@ -1,5 +1,136 @@
 // linked list class/factory
-const createLinkedList = (list) => {};
+const createLinkedList = (list = { value: 0, nextNode: null }) => {
+  const append = (node) => {
+    const currentTail = tail(list);
+    currentTail.nextNode = node;
+
+    // return list;
+  };
+
+  // Not modifying the list itself :((
+  // TODO: fix
+  const prepend = (node) => {
+    node.nextNode = list;
+    list = node;
+
+    // return list;
+  };
+
+  const size = (listCopy = list, level = 0) => {
+    if (listCopy.nextNode === null) return ++level;
+
+    return size(listCopy.nextNode, level + 1);
+  };
+
+  const tail = (list) => {
+    if (list.nextNode === null) return list;
+
+    return tail(list.nextNode);
+  };
+
+  const head = (list) => {
+    return list;
+  };
+
+  const at = (index, counter = 0, listCopy = list) => {
+    if (index === counter) return listCopy.value;
+    // TODO: error handling somewhere?
+    return at(index, counter + 1, listCopy.nextNode);
+  };
+
+  const getModifiedTail = (listCopy = list) => {
+    if (listCopy.nextNode.nextNode === null) return listCopy;
+
+    return getModifiedTail(listCopy.nextNode);
+  };
+
+  const pop = () => {
+    // get to the tail and remove the tail
+    // find the tail of the list
+    const nodeBeforeTail = getModifiedTail(list);
+
+    nodeBeforeTail.nextNode = null;
+    // console.log(nodeBeforeTail, tail);
+    return list;
+  };
+
+  const contains = (value, listCopy = list) => {
+    // go through each list.value (node), if it is there return true else false;
+    if (listCopy.value === value) return true;
+    if (listCopy.nextNode === null) return false;
+
+    return contains(value, listCopy.nextNode);
+  };
+
+  const find = (value, level = 0, listCopy = list) => {
+    if (listCopy.value === value) {
+      return level;
+    }
+    if (listCopy.nextNode === null && listCopy.value !== value) {
+      return `${value} not found`;
+    }
+
+    return find(value, level + 1, listCopy.nextNode);
+  };
+
+  const toString = (listCopy = list, nodeValue = '') => {
+    if (listCopy.nextNode === null)
+      return `${nodeValue}( ${listCopy.value} ) -> null`;
+
+    return toString(listCopy.nextNode, nodeValue + `( ${listCopy.value} ) -> `);
+  };
+
+  const insertAt = (value, index, listCopy = list, level = 0) => {
+    if (level === index - 1) {
+      // modify here
+      // APPROACH ONE:
+      // get the node before
+      const createdNode = createNode(value);
+      const transfer = listCopy.nextNode.nextNode;
+      listCopy.nextNode = createdNode;
+      createdNode.nextNode = transfer;
+      list = listCopy;
+
+      return listCopy;
+    }
+
+    return insertAt(value, index, listCopy.nextNode, level + 1);
+  };
+
+  // DEBUG
+  const removeAt = (index, listCopy = list, level = 0) => {
+    if (level === index) {
+      const transfer = listCopy.nextNode.nextNode;
+      listCopy.nextNode = transfer;
+      return listCopy;
+    }
+    if (level === index - 1) {
+      const transfer = listCopy.nextNode.nextNode;
+      listCopy.nextNode = transfer;
+      list = listCopy;
+
+      return listCopy;
+    }
+
+    return removeAt(index, listCopy.nextNode, level + 1);
+  };
+  return {
+    list,
+
+    append,
+    prepend,
+    size,
+    tail,
+    head,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+    insertAt,
+    removeAt,
+  };
+};
 
 const createNode = (val = null, next = null) => {
   const node = {
@@ -9,13 +140,6 @@ const createNode = (val = null, next = null) => {
 
   return node;
 };
-
-// const testArray = createLinkedList([12, 14]);
-// testArray.append(22);
-// console.log(testArray.head());
-// console.log(testArray.at(1));
-// console.log(testArray.tail());
-// console.log(testArray.pop());
 
 const linkedList = {
   value: 1,
@@ -162,11 +286,11 @@ const removeAt = (list, index, level = 0) => {
 // Extra Credit (done)
 
 // SAMPLE COMMANDS:
-append(linkedList, createNode(4));
+// append(linkedList, createNode(4));
 // showList(linkedList);
 // console.log(size(linkedList));
 
-const newList = prepend(linkedList, createNode(5));
+// const newList = prepend(linkedList, createNode(5));
 // showList(newList);
 // console.log(size(newList));
 // console.log(newList);
@@ -178,8 +302,28 @@ const newList = prepend(linkedList, createNode(5));
 // console.log(find(newList, 4));
 // console.log(toString(newList));
 
-showList(newList);
-console.log(insertAt(newList, 69, 2));
-showList(newList);
-console.log(removeAt(newList, 3));
-showList(newList);
+// showList(newList);
+// console.log(insertAt(newList, 69, 2));
+// showList(newList);
+// console.log(removeAt(newList, 3));
+// showList(newList);
+
+const testList = createLinkedList();
+testList.append(createNode(12));
+
+// console.log(testList.list);
+
+// console.log(testList.prepend(createNode(14)));
+testList.prepend(createNode(14));
+// prepend not modifying the list
+// testList.append(createNode(14));
+
+console.log('SIZE', testList.size());
+console.log('AT', testList.at(2));
+console.log('SIZE', testList.size());
+// console.log('POP', testList.pop());
+console.log('CONTAINS', testList.contains(68));
+console.log('FIND', testList.find(0));
+console.log('TOSTRING', testList.toString());
+console.log('INSERT AT', testList.insertAt(69, 1));
+console.log('REMOVE AT', testList.removeAt(0));
