afea1c532040fcc75d593ce150434573ce824313
theadg <deguzmanbernardandrew@gmail.com>
2023-01-04 18:17:35 +0800

    Remove test code

--- index.js
+++ index.js
@@ -145,21 +145,3 @@ const createNode = (val = null, next = null) => {
 
   return node;
 };
-const testList = linkedList();
-testList.append(createNode(12));
-
-testList.prepend(createNode(14));
-console.log(testList.list);
-console.log('SIZE', testList.size());
-console.log('AT', testList.at(2));
-// console.log('SIZE', testList.size());
-console.log('POP', testList.pop());
-console.log('SIZE', testList.size());
-
-console.log('CONTAINS', testList.contains(68));
-// console.log('FIND', testList.find(0));
-console.log('TOSTRING', testList.toString());
-// console.log('INSERT AT', testList.insertAt(69, 1));
-// console.log('TOSTRING', testList.toString());
-// console.log('REMOVE AT', testList.removeAt(1));
-// console.log('TOSTRING', testList.toString());
